2ddf777cee04ece7ce7fb1872f53cebc041d5fc0
namantia1@gmail.com <namantia1@gmail.com>
2021-01-29 16:53:22 -0500

    app.js setup / error desplayed when number guessed is out of range

---
+++ app.js
@@ -0,0 +1,34 @@
+// Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+//UI Elements
+const game = document.querySelector('#game'),
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
+  message = document.querySelector('.message');
+
+//Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener('click', function () {
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+  // Validate
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`please enter a number between ${min} and ${max}`);
+  }
+});
+
+//Set message
+
+const setMessage = (msg) => {
+  message.textContent = msg;
+};
